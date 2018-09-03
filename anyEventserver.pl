c5fb6ca4b4f695bc43261cc2bbb6a941f60d05b7
houspi <houspi@gmail.com>
2018-09-03 06:14:03 +0000

    update

--- anyEventserver.pl
+++ anyEventserver.pl
@@ -43,7 +43,8 @@ my $guard = tcp_server "0", $port,
             }
         );
         my $id = "$host:$port";
-        my $reader; $reader = sub {
+        my $reader; 
+        $reader = sub {
             my $data = $_[1];
             $input_data{$id} .= $_[1] . "\n";
             if ( $input_data{$id} =~ /\n\n/ ) {
